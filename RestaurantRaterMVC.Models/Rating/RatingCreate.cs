0dbac42abf9d8874aafd450c0c077fc3532f7162
Sam Page <samnpage96@gmail.com>
2023-12-04 16:42:39 -0500

    initial commit

---
+++ RestaurantRaterMVC.Models/Rating/RatingCreate.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterMVC.Models.Rating;
+public class RatingCreate
+{
+    [Required]
+    [Display(Name = "Restaurant")]
+    public int RestaurantId { get; set; }
+
+    [Required, Range(1, 5)]
+    [Display(Name = "Rating")]
+    public double? Score { get; set; }
+}
