af99b45e44389937d9d738c3de000e39c8bc8808
Sam Page <samnpage96@gmail.com>
2023-12-04 14:49:13 -0500

    GetRestaurantAsync method added

--- RestaurantRaterMVC.Services/Restaurant/IRestaurantService.cs
+++ RestaurantRaterMVC.Services/Restaurant/IRestaurantService.cs
@@ -4,6 +4,7 @@ namespace RestaurantRaterMVC.Services.Restaurant;
 public interface IRestaurantService
 {
     // Task<List<RestaurantListItem>> GetAllRestaurantsAsync();
-    Task<IEnumerable<RestaurantListItem>> GetAllRestaurantsAsync();
     Task<bool> CreateRestaurantAsync(RestaurantCreate model);
+    Task<IEnumerable<RestaurantListItem>> GetAllRestaurantsAsync();
+    Task<RestaurantDetail?> GetRestaurantAsync(int id);
 }
