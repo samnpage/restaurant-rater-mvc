db1727f37485a62827dadecc3e5f5be3d88f5e84
Sam Page <samnpage96@gmail.com>
2023-12-04 14:48:02 -0500

    initial commit

---
+++ RestaurantRaterMVC.Models/Restaurant/RestaurantDetail.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterMVC.Models.Restaurant;
+public class RestaurantDetail
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Location { get; set; }
+
+    [Display(Name = "Average Score")]
+    [DisplayFormat(DataFormatString = "[0:N2]")]
+    public double Score { get; set; }
+}
