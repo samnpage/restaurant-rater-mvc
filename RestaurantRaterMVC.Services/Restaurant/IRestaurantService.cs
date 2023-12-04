a93864ed1828b0f8544df8fc07deaa34ae32c586
Sam Page <samnpage96@gmail.com>
2023-12-04 15:35:41 -0500

    update and delete methods added

--- RestaurantRaterMVC.Services/Restaurant/IRestaurantService.cs
+++ RestaurantRaterMVC.Services/Restaurant/IRestaurantService.cs
@@ -3,8 +3,18 @@ using RestaurantRaterMVC.Models.Restaurant;
 namespace RestaurantRaterMVC.Services.Restaurant;
 public interface IRestaurantService
 {
-    // Task<List<RestaurantListItem>> GetAllRestaurantsAsync();
+    // Create
     Task<bool> CreateRestaurantAsync(RestaurantCreate model);
+
+    // Read All
     Task<IEnumerable<RestaurantListItem>> GetAllRestaurantsAsync();
+
+    // Read by Id
     Task<RestaurantDetail?> GetRestaurantAsync(int id);
+
+    // Update
+    Task<bool> UpdateRestaurantAsync(RestaurantEdit model);
+
+    // Delete
+    Task<bool> DeleteRestuarnatAsync(int id);
 }
