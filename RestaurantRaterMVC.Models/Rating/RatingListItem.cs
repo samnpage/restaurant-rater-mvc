ee3bcb463b24b997101f532586d142d622be70c4
Sam Page <samnpage96@gmail.com>
2023-12-04 15:55:23 -0500

    initial commit

---
+++ RestaurantRaterMVC.Models/Rating/RatingListItem.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterMVC.Models.Rating;
+public class RatingListItem
+{
+    [Display(Name = "Restaurant")]
+    public string? RestaurantName { get; set; }
+
+    [Display(Name = "Rating")]
+    public double? Score { get; set; }
+}
