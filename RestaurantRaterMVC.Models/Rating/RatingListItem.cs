f78ed1f4b18b3f466a6c91d7ce1f16abcf119668
Sam Page <samnpage96@gmail.com>
2023-12-06 17:39:39 -0500

    Id added

--- RestaurantRaterMVC.Models/Rating/RatingListItem.cs
+++ RestaurantRaterMVC.Models/Rating/RatingListItem.cs
@@ -3,6 +3,8 @@ using System.ComponentModel.DataAnnotations;
 namespace RestaurantRaterMVC.Models.Rating;
 public class RatingListItem
 {
+    public int Id {get; set;}
+    
     [Display(Name = "Restaurant")]
     public string? RestaurantName { get; set; }
 
